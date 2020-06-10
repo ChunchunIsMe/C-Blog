22971c64debcc520e0020e96187739c6b556c9ab
linlichun <linlichun@meicai.cn>
2020-06-10 19:59:29 +0800

    finish lazy

--- JavaScript/lazy/lazy.js
+++ JavaScript/lazy/lazy.js
@@ -3,3 +3,11 @@ function checkInWindow(dom) {
   const check = ((offset.top + offset.bottom) / 2 < window.innerHeight);
   return check;
 }
+const dom = document.getElementById('img');
+function run() {
+  if (checkInWindow(dom)) {
+    dom.setAttribute('src', './img/q.jpg');
+    window.removeEventListener('scroll', run);
+  }
+}
+window.addEventListener('scroll', run);
