e1c91c5dba61fb306d1855f0b13f9cecbf0b2e36
llc <843799751@qq.com>
2020-06-16 15:40:47 +0800

    finish balance tree

--- algorithm/tree/balanceTree/index.js
+++ algorithm/tree/balanceTree/index.js
@@ -1,13 +1,31 @@
 const { addSearchTreeNode, RemoveSearchTreeNode } = require('../searchTree/searchTree');
 
-function RR(node) {
+function getNodeHeight(node) {
+  if (node == null) {
+    return 0;
+  }
+  let left = 0, right = 0;
+  if (node.left) {
+    left = getNodeHeight(node.left)
+  }
+  if (node.right) {
+    right = getNodeHeight(node.right)
+  }
+  return Math.max(left, right) + 1;
+}
+
+function getBalance(node) {
+  return getNodeHeight(node.left) - getNodeHeight(node.right);
+}
+
+function LL(node) {
   const temp = node.left;
   node.left = temp.right;
   temp.right = node;
   return temp;
 }
 
-function LL(node) {
+function RR(node) {
   const temp = node.right;
   node.right = temp.left;
   temp.left = node;
@@ -15,87 +33,52 @@ function LL(node) {
 }
 
 function RL(node) {
-  node.right = RR(node.right);
+  node.left = RR(node.left);
   return LL(node);
 }
 
 function LR(node) {
-  node.left = LL(node.left);
+  node.right = LL(node.right);
   return RR(node);
 }
 
-
-function getHeight(node) {
-  if (node == undefined) {
-    return 0;
-  }
-  if (node.left == undefined && node.right == undefined) {
-    return 1;
-  }
-  return Math.max(getHeight(node.left), getHeight(node.right)) + 1;
-}
-
-function getBalance(node) {
-  if (node == undefined) {
-    return 0;
-  }
-  return getHeight(node.left) - getHeight(node.right);
-}
-
-function balanceTree(node, father = null) {
+function balanceTree(node) {
   const balance = getBalance(node);
-  const fatherBalance = getBalance(father);
-  if (balance > 1) {
-    if (fatherBalance < -1) {
-      father.right = RL(node)
+  if (balance < -1) {
+    const rightBalance = getBalance(node.right);
+    if (rightBalance < -1) {
+      node.right = balanceTree(node.right);
+    } else if (rightBalance <= 0) {
+      node = RR(node);
     } else {
-      node = balanceTree(node.left, node);
+      node = LR(node);
     }
-  } else if (balance < -1) {
-    if (fatherBalance > 1) {
-      father.left = LR(node);
+  } else if (balance > 1) {
+    const leftBalance = getBalance(node.left);
+    if (leftBalance > 1) {
+      node.left = balanceTree(node.left);
+    } else if (leftBalance >= 0) {
+      node = LL(node);
     } else {
-      node = balanceTree(node.right, node);
-    }
-  } else {
-    if (father === null) {
-      return node;
-    } else {
-      if (balance > 0 && fatherBalance > 1) {
-        return RR(node)
-      } else if (balance < 0 && fatherBalance < -1) {
-        return LL(node);
-      } else if (balance > 0 && fatherBalance < 0) {
-        return RL(node)
-      } else if (balance < 0 && fatherBalance > 0) {
-        return LR(node);
-      }
+      node = RL(node);
     }
   }
   return node;
 }
 
-function addBalanceNode(head, value) {
-  head = addSearchTreeNode(head, value);
-  head = balanceTree(head);
-  return head;
+function insertNode(node, value) {
+  if (node == null) {
+    return { value, left: undefined, right: undefined }
+  }
+  addSearchTreeNode(node, value);
+  return balanceTree(node);
 }
 
-function removeBalanceNode(head, value) {
-  head = removeBalanceNode(head, value);
-  head = balanceTree(head);
-  return head;
-}
-
-const a = addBalanceNode(undefined, 1)
-addBalanceNode(a, 2)
-addBalanceNode(a, 3)
-addBalanceNode(a, 4)
-addBalanceNode(a, 5)
-addBalanceNode(a, 6)
-addBalanceNode(a, 7)
-addBalanceNode(a, 8)
-addBalanceNode(a, 9)
-addBalanceNode(a, 10)
+let a = insertNode(null, 70);
+a = insertNode(a, 50);
+a = insertNode(a, 80);
+a = insertNode(a, 72);
+a = insertNode(a, 90);
 console.log(a);
-
+a = insertNode(a, 75);
+console.log(a);
